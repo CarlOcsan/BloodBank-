1c983980f800245ada2de9b961e508d045579326
CarlOcsan <c.ocsan010@mybvc.ca>
2022-04-22 20:15:46 -0600

    Blood bank

--- app/src/main/java/com/example/bloodbank/BloodBank.java
+++ app/src/main/java/com/example/bloodbank/BloodBank.java
@@ -1,7 +1,11 @@
 package com.example.bloodbank;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -11,6 +15,8 @@ public class BloodBank extends AppCompatActivity {
     private ImageView logo;
     private TextView title;
 
+    Animation firstAnimation, secAnimation;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,5 +25,22 @@ public class BloodBank extends AppCompatActivity {
 
         logo = findViewById(R.id.logo);
         title = findViewById(R.id.title);
+
+        firstAnimation = AnimationUtils.loadAnimation(this, R.anim.first_animation);
+        secAnimation = AnimationUtils.loadAnimation(this, R.anim.sec_animation);
+
+        logo.setAnimation(firstAnimation);
+        title.setAnimation(secAnimation);
+
+        int sScreen = 4300;
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(BloodBank.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, sScreen);
     }
 }
