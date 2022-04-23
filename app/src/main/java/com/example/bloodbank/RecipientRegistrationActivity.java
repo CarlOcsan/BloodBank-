134db21c669dcee8cfcba5351e06bb6b04b21064
CarlOcsan <c.ocsan010@mybvc.ca>
2022-04-22 22:12:33 -0600

    Blood bank

--- app/src/main/java/com/example/bloodbank/RecipientRegistrationActivity.java
+++ app/src/main/java/com/example/bloodbank/RecipientRegistrationActivity.java
@@ -1,13 +1,27 @@
 package com.example.bloodbank;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class RecipientRegistrationActivity extends AppCompatActivity {
+
+    private TextView backBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipient_registration);
+
+        backBtn = findViewById(R.id.backBtn);
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RecipientRegistrationActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/bloodbank/SelectRegActivity.java
+++ app/src/main/java/com/example/bloodbank/SelectRegActivity.java
@@ -1,13 +1,48 @@
 package com.example.bloodbank;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class SelectRegActivity extends AppCompatActivity {
+
+    private Button donorBtn, recipientBtn;
+    private TextView backBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_reg);
+
+        donorBtn = findViewById(R.id.donorBtn);
+        recipientBtn = findViewById(R.id.recipientBtn);
+        backBtn = findViewById(R.id.backBtn);
+
+        donorBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SelectRegActivity.this, DonorRegistrationActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        recipientBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SelectRegActivity.this, RecipientRegistrationActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SelectRegActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
