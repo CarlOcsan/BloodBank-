610a43808cbe0ff4b37215e52897b297ce57c765
CarlOcsan <c.ocsan010@mybvc.ca>
2022-04-22 22:27:04 -0600

    Blood bank

--- app/src/main/java/com/example/bloodbank/LoginActivity.java
+++ app/src/main/java/com/example/bloodbank/LoginActivity.java
@@ -5,19 +5,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private Button registerBtn;
+    private TextView backBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        registerBtn = findViewById(R.id.registerBtn);
+        backBtn = findViewById(R.id.backBtn);
 
-        registerBtn.setOnClickListener(new View.OnClickListener() {
+        backBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(LoginActivity.this, SelectRegActivity.class);
